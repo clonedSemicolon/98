9ea11ec22ed0387426971a208cef82f73f752b32
Avishak Chakroborty <aurgho1998@gmail.com>
2025-03-31 14:53:03 -0400

    Update ci_wrapper.sh

--- ci_wrapper.sh
+++ ci_wrapper.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Step 1: Checkout already done
-
-# Step 2: Setup node (done in Actions)
-
 echo "Installing dependencies"
 npm ci
 
 echo "Installing Playwright browsers"
 npx playwright install --with-deps
-
-echo "Running Playwright tests"
-npx playwright test
